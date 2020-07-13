cebe1aa4d68a0be215d908be699cb335e3b89a52
Jose Alvarez <alvarez_3993@hotmail.com>
2020-07-13 14:54:00 -0500

    feature(PetModel)

--- src/models/pets.model.ts
+++ src/models/pets.model.ts
@@ -3,6 +3,10 @@ import { DataTypes } from 'sequelize'
 import { connection } from '../connection'
 
 const pets = connection.define('Pets', { 
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false
